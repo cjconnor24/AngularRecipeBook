409885a513c3ae832e6a714e863b641b0e27112a
Chris Connor <cjconnor24@connordesign.com>
2018-03-21 11:49:07 +0000

    ADding a base route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,14 +6,13 @@ import { ShoppingListComponent } from '../shopping-list/shopping-list.component'
 
 // APP ROUTES GO HERE
 const appRoutes: Routes = [
+  {path: '', redirectTo: 'recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent},
   {path: 'shopping-list', component: ShoppingListComponent}
 ]
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
